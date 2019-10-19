f277716b1ef1648bc416776c440e006e0feda8cd
Sebastian Schreiber <sebschre@gmail.com>
2019-10-19 18:34:13 +0200

    contract changes

--- ethereum/contracts/Survey.sol
+++ ethereum/contracts/Survey.sol
@@ -1,96 +1,69 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.5.3;
 
-contract CampaignFactory {
-    address[] public deployedCampaigns;
+contract SurveyFactory {
+    Survey[] public deployedSurveys;
 
-    function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
-        deployedCampaigns.push(newCampaign);
+    function createSurvey() public {
+        Survey newSurvey = new Survey(msg.sender);
+        deployedSurveys.push(newSurvey);
     }
 
-    function getDeployedCampaigns() public view returns (address[]) {
-        return deployedCampaigns;
+    function getDeployedSurveys() public view returns (Survey[] memory) {
+        return deployedSurveys;
     }
 }
 
-contract Campaign {
-    struct Request {
-        string description;
-        uint value;
-        address recipient;
-        bool complete;
-        uint approvalCount;
-        mapping(address => bool) approvals;
-    }
-
-    Request[] public requests;
+contract Survey {
+    Question[] public questions;
     address public manager;
-    uint public minimumContribution;
-    mapping(address => bool) public approvers;
-    uint public approversCount;
 
-    modifier restricted() {
-        require(msg.sender == manager);
+    modifier restrictedToManager() {
+        require(msg.sender == manager, "Sender not authorized");
         _;
     }
 
-    function Campaign(uint minimum, address creator) public {
+    constructor (address creator) public {
         manager = creator;
-        minimumContribution = minimum;
     }
 
-    function contribute() public payable {
-        require(msg.value > minimumContribution);
-
-        approvers[msg.sender] = true;
-        approversCount++;
+    function createQuestion(string memory questionText) public restrictedToManager {
+        Question newQuestion = new QuestionClosedEnded(msg.sender, questionText);
+        questions.push(newQuestion);
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
-        });
+    function getQuestionsCount() public view returns (uint) {
+        return questions.length;
+    }
+}
 
-        requests.push(newRequest);
+contract Question {
+    address public survey;
+    string public questionText;
+    Reponse[] public reponses;
+
+    constructor (address creatorSurvey, string memory text) public {
+        survey = creatorSurvey;
+        questionText = text;
     }
 
-    function approveRequest(uint index) public {
-        Request storage request = requests[index];
+    function answer(Reponse) external;
+}
 
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
+contract Reponse {
+    address public respondant;
+    function setAnswer() public;
+}
 
-        request.approvals[msg.sender] = true;
-        request.approvalCount++;
+contract QuestionClosedEnded is Question {
+    string answerOptions;
+
+    constructor (address creatorSurvey, string memory text) Question(creatorSurvey, text) public {}
+
+    function answer() public {
+        answerOptions = "yes";
     }
+}
 
-    function finalizeRequest(uint index) public restricted {
-        Request storage request = requests[index];
+contract QuestionOpenEnded is Question {
 
-        require(request.approvalCount > (approversCount / 2));
-        require(!request.complete);
-
-        request.recipient.transfer(request.value);
-        request.complete = true;
-    }
-
-    function getSummary() public view returns (
-      uint, uint, uint, uint, address
-      ) {
-        return (
-          minimumContribution,
-          this.balance,
-          requests.length,
-          approversCount,
-          manager
-        );
-    }
-
-    function getRequestsCount() public view returns (uint) {
-        return requests.length;
-    }
 }
